5ee34a7064050804141785285a87f52f270f89e1
“[Grigory <[nwpgrifon@gmail.com]>
2021-12-23 12:35:57 +0300

    first commit

---
+++ lesson-01-servlet-api/src/main/java/ru/geekbrains/EncodingFilter.java
@@ -0,0 +1,15 @@
+package ru.geekbrains;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setContentType("text/html");
+        servletResponse.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
